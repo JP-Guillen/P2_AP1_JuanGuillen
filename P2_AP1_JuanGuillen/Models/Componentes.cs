17c7d0d2726b9811dfad61639df8388cadca7f14
JP-Guillen <juan_guillen@ucne.edu.do>
2025-11-03 18:44:46 -0400

    Agregando Modelos

---
+++ P2_AP1_JuanGuillen/Models/Componentes.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace P2_AP1_JuanGuillen.Models;
+
+public class Componentes
+{
+    [Key]
+    public int ComponenteId { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    public decimal Precio { get; set; }
+    public int Existencia { get; set; }
+
+}
---
+++ P2_AP1_JuanGuillen/Models/PedidosDetalles.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_AP1_JuanGuillen.Models;
+
+public class PedidosDetalles
+{
+    [Key]
+    public int Id { get; set; }
+    public int PedidoId { get; set; }
+    public int Componente { get; set; }
+    public int Cantidad { get; set; }
+    public float precio { get; set; }
+}
