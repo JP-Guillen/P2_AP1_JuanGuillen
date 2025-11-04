e2e1eef634169a50e10020502c3de1d1860645d2
JP-Guillen <juan_guillen@ucne.edu.do>
2025-11-03 21:36:58 -0400

    Todo lo nuevo

--- P2_AP1_JuanGuillen/Services/RegistroServices.cs
+++ P2_AP1_JuanGuillen/Services/RegistroServices.cs
@@ -16,5 +16,54 @@ public class RegistroServices(IDbContextFactory<Contexto> DbFactory)
             .AsNoTracking()
             .ToListAsync();
     }
+
+    public async Task<Pedidos?> Buscar(int pedidoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.pedidos
+            .Include(e => e.PedidosDetalles)
+                .ThenInclude(d => d.Componentes)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e => e.PedidoId == pedidoId);
+    }
+    public async Task<bool> Insertar(Pedidos pedidos)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.pedidos.Add(pedidos);
+
+        return await contexto.SaveChangesAsync() > 0;
+    }
+   
+    public async Task<bool> Modificar(Pedidos pedidos)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        var anterior = await contexto.pedidos
+            .Include(e => e.PedidosDetalles)
+            .FirstOrDefaultAsync(e => e.PedidoId == pedidos.PedidoId);
+
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Eliminar(int pedidoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+
+        var entrada = await contexto.pedidos
+            .Include(e => e.PedidosDetalles)
+            .FirstOrDefaultAsync(e => e.PedidoId == pedidoId);
+
+        return await contexto.SaveChangesAsync() > 0;
+    }
+    public async Task<bool> Existe(int pedidoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.pedidos
+            .AnyAsync(e => e.PedidoId == pedidoId);
+    }
+    public async Task<List<Componentes>> ListarComponentes()
+    {
+        using var contexto = DbFactory.CreateDbContext();
+        return await contexto.Componentes.ToListAsync();
+    }
 }
 
